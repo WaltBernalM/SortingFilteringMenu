e4376ec47ee0fc53190160e5bf807b79e339d853
Walter Bernal <wltrbm@gmail.com>
2023-10-09 00:23:52 -0700

    In Dessert class, creation of a function to retrieve all desserts consumed by "John Doe"

--- SortingFilteringMenu/Dessert+CoreDataProperties.swift
+++ SortingFilteringMenu/Dessert+CoreDataProperties.swift
@@ -31,6 +31,25 @@ extension Dessert {
     @NSManaged public var size: String?
     @NSManaged public var fromCustomer: NSSet?
 
+    // Function to retrieve all desserts consumed by "John Doe"
+    static func consumedByJohnDoe(_ context: NSManagedObjectContext) -> [Dessert]? {
+        guard let customer = Customer.with(fullName: "John Doe", context)
+        else { return nil }
+        
+        let request = Dessert.request()
+        
+        let predicate = NSPredicate(format: "(Any fromCustomer == %@", customer)
+        request.predicate = predicate
+        
+        do {
+            guard let results = try context.fetch(request) as? [Dessert], results.count > 0
+            else { return nil }
+            return results
+        } catch (let error) {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
 }
 
 // MARK: Generated accessors for fromCustomer
