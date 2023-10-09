13aaac79aa5802ca208c35f876be2347374e7ed3
Walter Bernal <wltrbm@gmail.com>
2023-10-09 00:22:11 -0700

    Function to search for a Customer with its fullName

--- SortingFilteringMenu/Customer+CoreDataProperties.swift
+++ SortingFilteringMenu/Customer+CoreDataProperties.swift
@@ -29,7 +29,22 @@ extension Customer {
     @NSManaged public var email: String?
     @NSManaged public var fullName: String?
     @NSManaged public var toDessert: NSSet?
-
+    
+    // Search for a Customer with its fullName
+    static func with( fullName: String, _ context: NSManagedObjectContext) -> Customer? {
+        let request = Customer.request()
+        let predicate  = NSPredicate(format: "fullName = %@", fullName)
+        request.predicate = predicate
+        do {
+            guard let results = try context.fetch(request) as? [Customer], results.count > 0
+            else { return nil }
+            return results.first
+        } catch (let error) {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
 }
 
 // MARK: Generated accessors for toDessert
---
+++ SortingFilteringMenu/Dessert+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Dessert+CoreDataClass.swift
+//  SortingFilteringMenu
+//
+//  Created by Walter Bernal Montero on 09/10/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Dessert)
+public class Dessert: NSManagedObject {
+
+}
